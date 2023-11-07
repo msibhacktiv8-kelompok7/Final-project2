ad0a8ff51b7e9901e0611c56eaf88c6524d1f949
aufall02 <aufaluzumaki@gmail.com>
2023-11-07 16:17:47 +0700

    chore : add relatiosn user adn photo

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.hasMany(models.photo)
+      this.hasMany(models.Photo, {as: "UserId"})
     }
   }
   User.init({
