015021bfbe0c3e7f802eefb590ef29574eca6bcf
Eko <ekoekofani@gmail.com>
2023-11-13 15:12:15 +0700

    add feat comments

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,21 @@ var express = require('express');
 var router = express.Router();
 const usersRoute = require('./users')
 const photoRoute = require('./photos')
+const commentsRoute = require('./comments')
 
 
 
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
   res.send('Hello welcome to my Photo Album Api')
 })
 // route users
 router.use('/users', usersRoute);
 // route photo
 router.use('/photos', photoRoute);
+// route photo
+router.use('/comments', commentsRoute);
 // handler route not found
-router.get('*', (req, res)=>{
+router.get('*', (req, res) => {
   res.status(404).send('Cari apa Hayooo???');
 })
 
