f500d873bfef6ca164d8333b465c4eb126f42ea3
aufall02 <aufaluzumaki@gmail.com>
2023-11-14 19:53:32 +0700

    change route

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,8 @@ const auth = require('../middleware/auth');
 var router = express.Router();
 
 
-router.post('/', UserController.register);
-router.post('/', UserController.login);
+router.post('/register', UserController.register);
+router.post('/login', UserController.login);
 router.put('/:userId',auth,UserController.update)
 router.delete('/:userId',auth,UserController.delete)
 
